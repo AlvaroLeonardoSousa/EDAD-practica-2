80f8838610e2f3b62bd05b677774d62d0659dcf5
Alvaro <alvaroleonardosousa@gmail.com>
2024-02-23 12:58:33 +0100

    P2_e1

--- p2_e1.c
+++ p2_e1.c
@@ -1,6 +1,10 @@
 #include "stack.h"
+#include "maze.h"
 #include <stdio.h>
 #include <limits.h>
+
+#define TOT_POINTS 3
+#define LEN_WORD 4
 
 void do_empty_stack (FILE *fp, Stack *s, print_elem_fn print_elem);
 
@@ -8,9 +12,46 @@ int char_print (FILE *fp, const void * elem);
 
 int main() {
 
+    int i;
+    Stack *stack;
+    char word[LEN_WORD];
 
+    Point *p[TOT_POINTS];
 
+    p[0] = point_new (0, 0, WALL);
+    p[1] = point_new (0, 1, SPACE);
+    p[2] = point_new (0, 2, IN);
 
+    stack = stack_new();
+
+    for (i=0; i<TOT_POINTS; i++) {
+
+        if(!p[i]) {
+            return 0;
+        }
+
+        if(stack_push(stack, p[i]) == ERROR) {
+            return 0;
+        }
+    }
+
+    stack_print(stdout, stack, point_print);
+
+    do_empty_stack(stdout, stack, point_print);
+
+    strcpy(word, 'hola');
+
+    for (i=0; i<LEN_WORD; i++) {
+        
+        if(stack_push(stack, (void*)word[i]) == ERROR) {
+            return 0;
+
+        }
+    }
+
+    stack_print(stdout, stack, char_print);
+
+    do_empty_stack(stdout, stack, char_print);
 
     return 0;
 }
